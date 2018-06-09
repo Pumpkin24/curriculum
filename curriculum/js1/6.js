70da43197faade23496d4e61580c0f20673dddfa
Alex Hsueh <pumpkin24@garagescript.org>
2018-06-09 16:00:02 -0700

    a solution for problem 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,17 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (x, counter = 2, sum = 0) => {
+  // base case: if x equals the counter, return the sum (of all divisors)
+  // if x can be evenly divided by counter, add to sum
+  // increment counter
+  // recurse by calling this function
+  //
+
+  if (x === counter) return sum;
+  if (x % counter === 0) sum = sum + counter;
+  counter = counter + 1;
+  return solution(x, counter, sum);
 };
 
 module.exports = {
